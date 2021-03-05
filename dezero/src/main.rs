b7d563da463f0bac62361d99132fe95a4d70e978
Tran Duc Son <sontd.hust@gmail.com>
2021-03-05 22:23:53 +0900

    Step 1.

--- dezero/src/main.rs
+++ dezero/src/main.rs
@@ -1,3 +1,13 @@
+use num_traits::Float;
+
+struct Variable<F: Float> {
+    data: F,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = 1.0;
+    let mut x = Variable { data };
+    println!("{}", x.data);
+    x.data = 2.0;
+    println!("{}", x.data);
 }
