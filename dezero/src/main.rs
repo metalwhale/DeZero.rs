f000b8f7ca9436093a815f389984fd88fd585320
Tran Duc Son <sontd.hust@gmail.com>
2021-03-07 01:18:53 +0900

    Step 4.

--- dezero/src/main.rs
+++ dezero/src/main.rs
@@ -43,13 +43,28 @@ impl Pass for Exp {
     }
 }
 
+fn numerical_diff<D: Float, F: Fn(Variable<D>) -> Variable<D>>(f: F, x: Variable<D>, eps: D) -> D {
+    let x0 = Variable { data: x.data - eps };
+    let x1 = Variable { data: x.data + eps };
+    let y0 = f(x0);
+    let y1 = f(x1);
+    return (y1.data - y0.data) / (D::from(2).unwrap() * eps);
+}
+
 fn main() {
+    const EPS: f64 = 1e-4;
     let a = Function::<Square>::default();
-    let b = Function::<Exp>::default();
-    let c = Function::<Square>::default();
+    let x = Variable { data: 2.0 };
+    let dy = numerical_diff(|v| a.call(v), x, EPS);
+    println!("{}", dy);
+
+    fn f<D: Float>(x: Variable<D>) -> Variable<D> {
+        let a = Function::<Square>::default();
+        let b = Function::<Exp>::default();
+        let c = Function::<Square>::default();
+        return c.call(b.call(a.call(x)));
+    }
     let x = Variable { data: 0.5 };
-    let a = a.call(x);
-    let b = b.call(a);
-    let y = c.call(b);
-    println!("{}", y.data);
+    let dy = numerical_diff(f, x, EPS);
+    println!("{}", dy);
 }
