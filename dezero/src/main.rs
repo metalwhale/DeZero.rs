c909564d4f2fc93e3b9710678d7fca55ecec6e0f
Tran Duc Son <sontd.hust@gmail.com>
2021-03-14 21:49:21 +0900

    Refactoring: Remove redundanciess; `backward` returns `Result`.

--- dezero/src/main.rs
+++ dezero/src/main.rs
@@ -7,36 +7,42 @@ struct Variable<'a, D: Float> {
     creator: Option<(Rc<RefCell<Variable<'a, D>>>, &'a dyn Function<D>)>,
 }
 impl<'a, D: Float> Variable<'a, D> {
-    fn new(data: D) -> Self {
-        Self {
+    fn new(data: D) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
             data,
             grad: None,
             creator: None,
-        }
+        }))
     }
 
     fn set_creator(&mut self, var: Rc<RefCell<Variable<'a, D>>>, func: &'a dyn Function<D>) {
         self.creator = Some((var, func));
     }
 
-    fn backward(&mut self) {
-        if let Some((ref mut x, p)) = self.creator {
-            let mut x = x.borrow_mut();
-            x.grad = Some(p.backward(x.data, self.grad.unwrap()));
-            x.backward();
+    fn backward(&self) -> Result<(), ()> {
+        match self.creator {
+            Some((ref x, f)) => match self.grad {
+                Some(grad) => {
+                    let mut x = x.borrow_mut();
+                    x.grad = Some(f.backward(x.data, grad));
+                    x.backward()
+                }
+                None => Err(()),
+            },
+            None => Ok(()),
         }
     }
 }
 
 trait Function<D: Float> {
-    fn call<'a>(&'a mut self, input: Rc<RefCell<Variable<'a, D>>>) -> Variable<'a, D>
+    fn call<'a>(&'a self, input: Rc<RefCell<Variable<'a, D>>>) -> Rc<RefCell<Variable<'a, D>>>
     where
         Self: Sized,
     {
         let x = input.borrow().data;
         let y = self.forward(x);
-        let mut output = Variable::new(y);
-        output.set_creator(input, self);
+        let output = Variable::new(y);
+        output.borrow_mut().set_creator(input, self);
         return output;
     }
 
@@ -44,7 +50,6 @@ trait Function<D: Float> {
     fn backward(&self, x: D, gy: D) -> D;
 }
 
-#[derive(Default)]
 struct Square {}
 impl<D: Float> Function<D> for Square {
     fn forward(&self, x: D) -> D {
@@ -57,7 +62,6 @@ impl<D: Float> Function<D> for Square {
     }
 }
 
-#[derive(Default)]
 struct Exp {}
 impl<D: Float> Function<D> for Exp {
     fn forward(&self, x: D) -> D {
@@ -71,14 +75,16 @@ impl<D: Float> Function<D> for Exp {
 }
 
 fn main() {
-    let mut a = Square::default();
-    let mut b = Exp::default();
-    let mut c = Square::default();
-    let x = Rc::new(RefCell::new(Variable::new(0.5)));
-    let m = Rc::new(RefCell::new(a.call(x.clone())));
-    let n = Rc::new(RefCell::new(b.call(m.clone())));
-    let mut y = c.call(n.clone());
+    let a = Square {};
+    let b = Exp {};
+    let c = Square {};
+    let x = Variable::new(0.5);
+    let m = a.call(x.clone());
+    let n = b.call(m.clone());
+    let y = c.call(n.clone());
+    let mut y = y.borrow_mut();
     y.grad = Some(1.0);
-    y.backward();
-    println!("{}", x.clone().borrow().grad.unwrap());
+    if let Ok(_) = y.backward() {
+        println!("{}", x.clone().borrow().grad.unwrap());
+    }
 }
