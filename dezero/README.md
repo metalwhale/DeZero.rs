852071c34aa832b532ea1c1622c59cda39ce3cd6
Tran Duc Son <sontd.hust@gmail.com>
2021-03-30 22:04:08 +0900

    Step 11, 12, 13.

--- dezero/README.md
+++ dezero/README.md
@@ -4,3 +4,7 @@
 ```
 $ cargo test
 ```
+
+## Todo
+- Step 11: Return multiple outputs.
+- Step 12: Accommodate to variadic arguments and return values.
--- dezero/src/lib.rs
+++ dezero/src/lib.rs
@@ -17,20 +17,24 @@ impl<'c, N: Float> Variable<'c, N> {
     }
 
     pub fn backward(&self) {
-        let mut calcs = vec![];
-        let mut x = self;
-        let mut grad = match self.grad.borrow().clone() {
+        let mut inputs = vec![self];
+        let mut gxs = vec![match self.grad.borrow().clone() {
             Some(g) => g,
-            None => ArrayD::ones(x.data.raw_dim()),
-        };
+            None => ArrayD::ones(self.data.raw_dim()),
+        }];
+        let mut calcs = vec![];
         loop {
-            x.grad.replace(Some(grad.clone()));
-            if let Some(creator) = &x.creator {
-                calcs.push((creator, grad));
-            }
-            if let Some((Calculation { input, function: f }, gy)) = calcs.pop() {
-                x = input;
-                grad = f.backward(&x.data, &gy);
+            inputs.iter().zip(gxs.into_iter()).for_each(|(i, gx)| {
+                i.grad.replace(Some(gx.clone()));
+                if let Some(creator) = &i.creator {
+                    calcs.push((creator, gx));
+                }
+            });
+            if let Some((c, gy)) = calcs.pop() {
+                inputs = c.inputs.to_vec();
+                gxs = c
+                    .function
+                    .backward(&inputs.iter().map(|i| &i.data).collect::<Vec<_>>(), &gy);
             } else {
                 break;
             }
@@ -39,112 +43,79 @@ impl<'c, N: Float> Variable<'c, N> {
 }
 
 trait Function<N: Float> {
-    fn call<'c>(&'c self, input: &'c Variable<'c, N>) -> Variable<'c, N>
+    fn call<'c>(&'c self, inputs: &[&'c Variable<'c, N>]) -> Variable<'c, N>
     where
         Self: Sized,
     {
-        let x = &input.data;
-        let y = self.forward(x);
+        let xs = inputs.iter().map(|i| &i.data).collect::<Vec<_>>();
+        let y = self.forward(&xs);
         let output = Variable::new(
             y,
             Some(Calculation {
-                input,
+                inputs: inputs.to_vec(),
                 function: self,
             }),
         );
         return output;
     }
 
-    fn forward(&self, x: &ArrayD<N>) -> ArrayD<N>;
-    fn backward(&self, x: &ArrayD<N>, gy: &ArrayD<N>) -> ArrayD<N>;
+    fn forward(&self, xs: &[&ArrayD<N>]) -> ArrayD<N>;
+    fn backward(&self, xs: &[&ArrayD<N>], gy: &ArrayD<N>) -> Vec<ArrayD<N>>;
 }
 
 pub struct Calculation<'c, N: Float> {
-    input: &'c Variable<'c, N>,
+    inputs: Vec<&'c Variable<'c, N>>,
     function: &'c dyn Function<N>,
 }
 
 struct Square;
 impl<N: Float> Function<N> for Square {
-    fn forward(&self, x: &ArrayD<N>) -> ArrayD<N> {
-        return x.mapv(|n| n.powi(2));
+    fn forward(&self, xs: &[&ArrayD<N>]) -> ArrayD<N> {
+        let y = xs[0].mapv(|n| n.powi(2));
+        return y;
     }
 
-    fn backward(&self, x: &ArrayD<N>, gy: &ArrayD<N>) -> ArrayD<N> {
+    fn backward(&self, xs: &[&ArrayD<N>], gy: &ArrayD<N>) -> Vec<ArrayD<N>> {
+        let x = xs[0];
         let two = N::from(2).unwrap();
         let gx = x.mapv(|n| two * n) * gy;
-        return gx;
+        return vec![gx];
     }
 }
-
-struct Exp;
-impl<N: Float> Function<N> for Exp {
-    fn forward(&self, x: &ArrayD<N>) -> ArrayD<N> {
-        return x.mapv(N::exp);
-    }
-
-    fn backward(&self, x: &ArrayD<N>, gy: &ArrayD<N>) -> ArrayD<N> {
-        let gx = x.mapv(N::exp) * gy;
-        return gx;
-    }
-}
-
 pub fn square<'c, N: Float>(x: &'c Variable<'c, N>) -> Variable<'c, N> {
-    return Square.call(x);
+    return Square.call(&[x]);
 }
 
-pub fn exp<'c, N: Float>(x: &'c Variable<'c, N>) -> Variable<'c, N> {
-    return Exp.call(x);
+struct Add;
+impl<N: Float> Function<N> for Add {
+    fn forward(&self, xs: &[&ArrayD<N>]) -> ArrayD<N> {
+        let y = xs[0] + xs[1];
+        return y;
+    }
+
+    fn backward(&self, _xs: &[&ArrayD<N>], gy: &ArrayD<N>) -> Vec<ArrayD<N>> {
+        return vec![gy.clone(), gy.clone()];
+    }
+}
+pub fn add<'c, N: Float>(x0: &'c Variable<'c, N>, x1: &'c Variable<'c, N>) -> Variable<'c, N> {
+    return Add.call(&[x0, x1]);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use ndarray::arr0;
-    use ndarray_rand::{rand_distr::Uniform, RandomExt};
-
-    fn numerical_diff<N: Float, F: for<'c> Fn(&'c Variable<'c, N>) -> Variable<'c, N>>(
-        f: F,
-        x: &Variable<N>,
-        eps: N,
-    ) -> ArrayD<N> {
-        let x0 = Variable::new(x.data.mapv(|n| n - eps), None);
-        let x1 = Variable::new(x.data.mapv(|n| n + eps), None);
-        let y0 = f(&x0);
-        let y1 = f(&x1);
-        let two = N::from(2).unwrap();
-        return (y1.data - y0.data).mapv(|n| n / (two * eps));
-    }
 
     #[test]
-    fn square_forward() {
+    fn test() {
         let x = Variable::new(arr0(2.0), None);
-        let y = square(&x);
-        let expected = arr0(4.0).into_dyn();
-        assert_eq!(&y.data, &expected);
-    }
-
-    #[test]
-    fn square_backward() {
-        let x = Variable::new(arr0(3.0), None);
-        let y = square(&x);
-        y.backward();
-        let expected = arr0(6.0).into_dyn();
-        assert_eq!(x.grad.borrow().as_ref().unwrap(), &expected);
-    }
-
-    #[test]
-    fn square_gradient_check() {
-        let x = Variable::new(Array::random((), Uniform::new(0.0, 1.0)), None);
-        let y = square(&x);
-        y.backward();
-        let num_grad = numerical_diff(square, &x, 1e-4);
-        let flg = x
-            .grad
-            .borrow()
-            .as_ref()
-            .unwrap()
-            .abs_diff_eq(&num_grad, 1e-8);
-        assert!(flg);
+        let y = Variable::new(arr0(3.0), None);
+        let sx = square(&x);
+        let sy = square(&y);
+        let z = add(&sx, &sy);
+        z.backward();
+        println!("{}", z.data);
+        println!("{}", x.grad.borrow().as_ref().unwrap());
+        println!("{}", y.grad.borrow().as_ref().unwrap());
     }
 }
